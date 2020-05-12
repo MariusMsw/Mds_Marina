65fcec1a9f4d2bc42b248dfb43b3ecce40602a94
Mihai Marius <marius.mihai@triplemars.com>
2020-05-13 00:16:02 +0300

    fourth commit

--- src/main/java/com/mds/backendmdsmarina/BackendMdsMarinaApplication.java
+++ src/main/java/com/mds/backendmdsmarina/BackendMdsMarinaApplication.java
@@ -2,12 +2,32 @@ package com.mds.backendmdsmarina;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class BackendMdsMarinaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendMdsMarinaApplication.class, args);
+	}
+
+	@Bean
+	public FilterRegistrationBean corsFilterRegistrationBean() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.addAllowedOrigin("*");
+		config.addAllowedHeader("*");
+		config.addAllowedMethod("*");
+		source.registerCorsConfiguration("/**", config);
+		FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+		bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+		return bean;
 	}
 
 }
